6ff9c3a2a273b946bf49b79a24f8b8d04e3cecde
Mack <macknz7@gmail.com>
2021-10-31 02:14:46 -0400

    local functions and operators, delegates and events, implementing interfaces, generics, and deriving types using inheritance and OOP. You also learned about base and derived classes, and how to override a type member, use polymorphism, and cast between types.

---
+++ code/chapter6/PacktLibrary/Squarer.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading;
+
+namespace Packt.Shared
+{
+    public class Squarer
+    {
+        public static double Square<T>(T input) where T : IConvertible
+        {
+            double d = input.ToDouble(Thread.CurrentThread.CurrentCulture);
+            return d * d;
+        }
+    }
+}
