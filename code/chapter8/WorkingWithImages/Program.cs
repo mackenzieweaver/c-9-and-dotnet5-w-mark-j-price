1bb242ecd2146028112b721e05ffba9b166ccb73
Mack <macknz7@gmail.com>
2021-11-01 22:33:04 -0400

    chapter 8
    regular expressions
    lists, sorted lists, dictionaries
    spans, indexes, ranges
    reflection, assemblies, types, methods
    localization, internationalization, globalization
    network stuff, ip addresses, protocol, domain (host), port, path, query params
    images, reading, manipulating, and writing

---
+++ code/chapter8/WorkingWithImages/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace WorkingWithImages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imagesFolder = Path.Combine(Environment.CurrentDirectory, "img");
+            IEnumerable<string> images = Directory.EnumerateFiles(imagesFolder);
+            foreach (string imagePath in images)
+            {
+                string thumbnailPath = Path.Combine(Environment.CurrentDirectory, "img", Path.GetFileNameWithoutExtension(imagePath) + "-thumbnail" + Path.GetExtension(imagePath));
+                using (Image image = Image.Load(imagePath))
+                {
+                    image.Mutate(x => x.Resize(image.Width / 10, image.Height / 10));
+                    image.Mutate(x => x.Grayscale());
+                    image.Save(thumbnailPath);
+                }
+            }
+        }
+    }
+}
---
+++ code/chapter8/WorkingWithText/Program.cs
@@ -0,0 +1,47 @@
+﻿using static System.Console;
+using System;
+
+namespace WorkingWithText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = "London";
+            WriteLine($"{city} is {city.Length} characters long.");
+
+            string cities = "Paris,Berlin,Madrid,New York";
+            string[] citiesArray = cities.Split(',');
+            foreach (string item in citiesArray)
+            {
+                WriteLine(item);
+            }
+
+            string fullName = "Alan Jones";
+            int indexOfTheSpace = fullName.IndexOf(' ');
+            string firstName = fullName.Substring(
+                startIndex: 0, length: indexOfTheSpace);
+            string lastName = fullName.Substring(
+                startIndex: indexOfTheSpace + 1);
+            string lastThenFirst = $"{lastName}, {firstName}";
+            WriteLine(lastThenFirst);
+            string firstThenLast = $"{firstName} {lastName}";
+            WriteLine(firstThenLast);
+
+            string company = "Microsoft";
+            bool startsWithM = company.StartsWith("M");
+            bool containsN = company.Contains("N");
+            WriteLine($"Starts with M: {startsWithM}, contains an N: {containsN}");
+
+            string recombined = string.Join(" => ", citiesArray);
+            WriteLine(recombined);
+
+            string fruit = "Apples";
+            decimal price = 0.39M;
+            DateTime when = DateTime.Today;
+            WriteLine($"{fruit} cost {price:C} on {when:dddd}s.");
+            WriteLine(string.Format("{0} cost {1:C} on {2:dddd}s.",
+            fruit, price, when));
+        }
+    }
+}
