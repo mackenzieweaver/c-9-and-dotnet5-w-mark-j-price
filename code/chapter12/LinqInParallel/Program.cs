2b5498f6a5052b125acf948ef8dd1d19b47c35df
Mack <macknz7@gmail.com>
2021-11-06 00:35:09 -0400

    LINQ queries
    select, project, filter, sort, join, and
    group data in many different formats, including XML

---
+++ code/chapter12/LinqInParallel/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace LinqInParallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = new Stopwatch();
+            Write("Press ENTER to start: ");
+            ReadLine();
+
+            watch.Start();
+            IEnumerable<int> numbers = Enumerable.Range(1, 2_000_000_000);
+            var squares = numbers
+                .AsParallel()
+                .Select(number => number * number).ToArray();
+            watch.Stop();
+
+            WriteLine("{0:#,##0} elapsed milliseconds.", arg0: watch.ElapsedMilliseconds);
+        }
+    }
+}
