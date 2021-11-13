3ba9d0e8bbc1403f3716760475f93b1361af698d
Mackenzie Weaver <macknz7@gmail.com>
2021-11-13 15:27:11 -0500

    cms
    enable developers to rapidly build websites
    that non-technical users can use to create and manage their own content.

---
+++ code/PracticalApps/NorthwindCms/Models/ExploreBlocks.cs
@@ -0,0 +1,15 @@
+using Piranha.Extend.Blocks;
+
+class ExploreBlocks
+{
+    AudioBlock ab;
+    HtmlBlock hb;
+    ColumnBlock cb;
+    ImageBlock ib;
+    ImageGalleryBlock igb;
+    PageBlock pb;
+    QuoteBlock qb;
+    SeparatorBlock sb;
+    TextBlock tb;
+    VideoBlock vb;
+}
